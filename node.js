f5bde32c3e124583e5c553ad7f127738aa988dd8
Ben Sanders <ben@sanders.life>
2023-01-07 17:04:00 -0500

    Added the old NodeJS webserver code for fun

---
+++ node.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
+// Old NodeJS code taken from site https://web.archive.org/web/20120530182534/http://nodejs.org/
